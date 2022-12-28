060daf0d5e10c6383fdd82b3519497088c9485ab
ali <ali@MacBook-Pro-Ali.local>
2022-12-28 14:28:10 +0400

    mesaj

---
+++ hometask1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace hometask1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("ededi daxil et:");
+        int number = int.Parse(Console.ReadLine());
+        int r = 0;
+        int sum = 0;
+
+        while (number >= 10)
+
+        {
+            r = number % 10;
+            number /= 100;
+            sum += r * r;
+
+
+        }Console.WriteLine(sum); 
+
+    }
+}
+
